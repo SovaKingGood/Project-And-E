99847b64fa21d9a80bc4a556dc4e1fe0dbe0d4c4
Brennan Pinette <130514371+SovaKingGood@users.noreply.github.com>
2025-11-19 15:20:25 -0800

    PS4, UI, Touch Work

--- And-E Display/Controller.h
+++
@@ -1,9 +0,0 @@
-//Display
-#include <Bluepad32.h>
-
-void setupController();
-void pollController();
-void onConnectedController(ControllerPtr ctl);
-void onDisconnectedController(ControllerPtr ctl);
-
-extern ControllerPtr myControllers[1];
